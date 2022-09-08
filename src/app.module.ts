aea12e64c990379aa12420151e1ab89ec38cafc3
João Pacheco <jrpacheco.dev@gmail.com>
2022-09-08 17:21:08 +0100

    bump

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,20 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLISODateTime } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       typePaths: ['./**/*.graphql'], //combine all graphql files in the project
       playground: false, //disable playground
       plugins: [ApolloServerPluginLandingPageLocalDefault()], //enable playground
+      driver: ApolloDriver,
+      resolvers: { DateTime: GraphQLISODateTime },
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
